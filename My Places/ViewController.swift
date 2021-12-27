f9727f7e0901cf0c89731492ca53e9c9e6dec947
Dima <drdima68@yandex.ua>
2021-12-27 18:55:33 +0300

    Initial Commit

---
+++ My Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by Дмитрий Татаринцев on 27.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
